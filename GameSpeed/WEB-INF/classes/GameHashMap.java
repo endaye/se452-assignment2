9dd18912fdd21b435d2abeefe6508cfd8fd36e56
Yuancheng Zhang <old.far.hot@gmail.com>
2016-04-24 19:32:53 -0500

    added game list  import from son file

--- GameSpeed/WEB-INF/classes/GameHashMap.java
+++ GameSpeed/WEB-INF/classes/GameHashMap.java
@@ -1,4 +1,7 @@
+import java.io.*;
+import java.util.Iterator;
 
+import org.json.*;
 
 import java.util.HashMap;
 
@@ -7,31 +10,73 @@ public class GameHashMap {
 	public static HashMap<String, Game> electronicArts = new HashMap<String, Game>();
 	public static HashMap<String, Game> activision = new HashMap<String, Game>();
 	public static HashMap<String, Game> takeTwoInteractive = new HashMap<String, Game>();
-	
+    public static HashMap<String, Game> allGames = new HashMap<String, Game>();
+    
 	public static String string_electronicArts = "Electronic Arts";
 	public static String string_activision = "Activision";
 	public static String string_takeTwoInteractive = "Take-Two Interactive";
 	
 	public GameHashMap() {
-		if(electronicArts.isEmpty()){
-			Game ea_fifa = new Game("FIFA 2016",59.99,"ea_fifa.jpg","Electronic Arts","New",10);
-			Game ea_nfs = new Game("Need for Speed",59.99,"ea_nfs.jpg","Electronic Arts","New",10);
-
-			
-			electronicArts.put("ea_nfs", ea_nfs);
-			electronicArts.put("ea_fifa", ea_fifa);
-		}
-		if(activision.isEmpty()){
-			Game activision_cod = new Game("Call Of Duty",54.99,"activision_cod.jpg","Activision","New",10);
-
-			activision.put("activision_cod", activision_cod);
-			
-		}
-		if(takeTwoInteractive.isEmpty()){
-
-			Game tti_evolve = new Game("Evolve",49.99,"tti_evolve.jpg","Take-Two Interactive","New",10);
-			
-			takeTwoInteractive.put("tti_evolve", tti_evolve);
-		}
+		this.getGameList();
+        if (!allGames.isEmpty()) {
+            for (HashMap.Entry<String, Game> entry : allGames.entrySet()) {
+                String key = entry.getKey();
+                Game value = entry.getValue();
+                if (value.getRetailer().equals(string_electronicArts)) {
+                    electronicArts.put(key, value);
+                } else if (value.getRetailer().equals(string_activision)) {
+                    activision.put(key, value);
+                } else if (value.getRetailer().equals(string_takeTwoInteractive)) {
+                    takeTwoInteractive.put(key, value);
+                } else {
+                    // do nothing
+                }
+            }
+        }
+        
 	}
+
+	public void getGameList() {
+        try {
+            JSONObject jsonObject = new JSONObject(readFile(
+                    "/Library/Tomcat/webapps/GameSpeed/gamelist.json"));
+            JSONArray jsonGames = (JSONArray) jsonObject.get("games");
+
+            Iterator<Object> it = jsonGames.iterator();
+            while (it.hasNext()) {
+                JSONObject jsonTemp = (JSONObject) it.next();
+                String name      = (String) jsonTemp.get("name");
+                double price     = (double) jsonTemp.get("price");
+                String image     = (String) jsonTemp.get("image");
+                String retailer  = (String) jsonTemp.get("retailer");
+                String condition = (String) jsonTemp.get("condition");
+                double discount  = (double) jsonTemp.get("discount");
+                Game gameItem = new Game(name, price, image, retailer, condition, discount);
+                
+                if (!allGames.containsKey(gameItem)) {
+                    allGames.put(name, gameItem);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readFile(String filename) {
+        String result = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
+            }
+            result = sb.toString();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
