4a5c1b49e30125b463beba6de8fbb34d5b1ef6e5
Yuancheng Zhang <old.far.hot@gmail.com>
2016-05-01 04:36:11 -0500

    update game list layout

--- GameSpeed2U/src/GamesList.java
+++ GameSpeed2U/src/GamesList.java
@@ -10,66 +10,63 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "GameeList", urlPatterns = "/GamesList")
+@WebServlet(name = "GameList", urlPatterns = "/GamesList")
 public class GamesList extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter pw = response.getWriter();
-				
-		String name = null;
-		String CategoryName = request.getParameter("maker");
-		HashMap<String, Game> hm = new HashMap<String, Game>();
-		
-		if(CategoryName==null){
-			hm.putAll(GameHashMap.electronicArts);
-			hm.putAll(GameHashMap.activision);
-			hm.putAll(GameHashMap.takeTwoInteractive);
-			name = "";
-		}else{
-		if(CategoryName.equals("electronicArts")){
-			hm.putAll(GameHashMap.electronicArts);
-			name = GameHashMap.string_electronicArts;
-		}
-		else if(CategoryName.equals("activision")){
-			hm.putAll(GameHashMap.activision);
-			name = GameHashMap.string_activision;
-		}
-		else if(CategoryName.equals("takeTwoInteractive")){
-			hm.putAll(GameHashMap.takeTwoInteractive);
-			name = GameHashMap.string_takeTwoInteractive;
-		}
-		}
-		
-		Helper helper = new Helper(request,pw);
-		helper.printHtml("site_header.html");
-		pw.print("<div id='content'><div class='post'><h2 class='title meta'>");
-		pw.print("<a style='font-size: 24px;'>"+name+" Games</a>");
-        pw.print("</h2><div class='entry'><table id='bestseller'>");
-        int i = 1; int size= hm.size();
-		for(Map.Entry<String, Game> entry : hm.entrySet()){
-			Game game = entry.getValue();
-			if(i%3==1) pw.print("<tr>");
-			pw.print("<td><div id='shop_item'>");
-			pw.print("<h3>"+game.getName()+"</h3>");
-			pw.print("<strong>"+ "$" + game.getPrice() + "</strong><ul>");
-			pw.print("<li id='item'><img src='images/games/"+game.getImage()+"' alt='' /></li>");
-			pw.print("<li><form method='post' action='Cart'>" +
-					"<input type='hidden' name='name' value='"+entry.getKey()+"'>"+
-					"<input type='hidden' name='type' value='games'>"+
-					"<input type='hidden' name='maker' value='"+CategoryName+"'>"+
-					"<input type='hidden' name='access' value=''>"+
-					"<input type='submit' class='btnbuy' value='Buy Now' href='#'></input></form></li>");
-			pw.print("<li><a class='btnreview' href='Review?name="+entry.getKey()+"&type=games&maker="+CategoryName+"&access='>Reviews</a></li>");
-			
-			pw.print("</ul></div></td>");
-			if(i%3==0 || i == size) pw.print("</tr>");
-			i++;
-		}		
-		pw.print("</table></div></div></div>");
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
 
-		helper.printHtml("site_sidebar.html");
-		helper.printHtml("site_footer.html");
-	}
+        String name = null;
+        String CategoryName = request.getParameter("maker");
+        HashMap<String, Game> hm = new HashMap<String, Game>();
+
+        if (CategoryName==null) {
+            hm.putAll(GameHashMap.electronicArts);
+            hm.putAll(GameHashMap.activision);
+            hm.putAll(GameHashMap.takeTwoInteractive);
+            name = "";
+        } else {
+            if(CategoryName.equals("electronicArts")) {
+                hm.putAll(GameHashMap.electronicArts);
+                name = GameHashMap.string_electronicArts;
+            }
+            else if (CategoryName.equals("activision")) {
+                hm.putAll(GameHashMap.activision);
+                name = GameHashMap.string_activision;
+            }
+            else if (CategoryName.equals("takeTwoInteractive")) {
+                hm.putAll(GameHashMap.takeTwoInteractive);
+                name = GameHashMap.string_takeTwoInteractive;
+            }
+        }
+
+        Helper helper = new Helper(request,pw);
+        helper.printHtml("site_header.html");
+        pw.print("<div id='body'><section id='content'><article class='expanded'>");
+        pw.print("<h2>"+name+" Games</h2>");
+        for(Map.Entry<String, Game> entry : hm.entrySet()){
+            Game game = entry.getValue();
+            pw.print(showGameItemHtml(game.getName(), game.getPrice(), game.getImage()));
+        }
+        pw.print("</article></section>");
+
+        helper.printHtml("site_sidebar.html");
+        helper.printHtml("site_footer.html");
+    }
+
+    private String showGameItemHtml(String name, double price, String image) {
+        String out = "";
+        out += "<div class='item'><div class='item-image'>" +
+                "<img src='images/games/"+ image + "' " +
+                "alt='"+ name +"'/></div>" +
+                "<div class='item-title'>" +
+                "<h3>" + name + "</h3><strong>$" + price + "</strong></div>" +
+                "<div class='item-detail'><ul>" +
+                "<li><span class='item-button'><a class='button' href='#'>Buy Now</a></span></li>" +
+                "<li><span class='item-button'><a class='button' href='#'>Reviews</a></span></li>" +
+                "</ul></div></div>";
+        return out;
+    }
 }
